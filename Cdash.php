c977432803f56f2f6d940d7801584d6b2a2ffe24
Sanga024 <bayzidsanga2223@gmail.com>
2026-01-18 15:12:52 +0600

    Updated

---
+++ Cdash.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Cdash.css">
+    
+</head>
+<body>
+    <div class="side-bar">
+        <h1>Dashboard</h1>
+        <ul>
+            <li><a href="">Catagories</a></li>
+            <li><a href="">View Cart</a></li>
+            <li><a href="">Offers</a></li>
+            <li><a href="">Wishlist</a></li>
+            <li><a href="">Payment Policy</a></li>
+            <li><a href="">Log out</a></li>
+
+            
+        </ul>
+    </div>
+    <div class="content">
+        <img src="SAVYZ TEXT LOGO.png">
+        <div class="srcbar">
+            <input type="search" placeholder="Search">
+
+
+        </div>
+        
+            
+
+        
+    </div>
+    <div class="main">
+
+    <div class="procard">
+        <div class="product-img">
+           <img src="R6.jpeg">
+        </div>
+        <h3>Rainbow Six</h3>
+        <p>200 BDT</p>
+        <button>Add to Cart</button>
+    </div>
+
+    <div class="procard">
+        <div class="product-img">
+            <img src="naruto.webp">
+        </div>
+        <h3>Naruto</h3>
+        <p>350 BDT</p>
+        <button>Add to Cart</button>
+    </div>
+
+    <div class="procard">
+        <div class="product-img">
+            <img src="luffy.webp">
+        </div>
+        <h3>Luffy</h3>
+        <p>300 BDT</p>
+        <button>Add to Cart</button>
+    </div>
+
+    <div class="procard">
+        <div class="product-img">
+            <img src="sasuke.jpg">
+        </div>
+        <h3>Sasuke</h3>
+        <p>320 BDT</p>
+        <button>Add to Cart</button>
+    </div>
+
+    <div class="procard">
+        <div class="product-img">
+            <img src="goku.jpg">
+        </div>
+        <h3>Goku</h3>
+        <p>400 BDT</p>
+        <button>Add to Cart</button>
+    </div>
+
+    <div class="procard">
+        <div class="product-img">
+            <img src="ichigo.jpeg">
+        </div>
+        <h3>Ichigo</h3>
+        <p>280 BDT</p>
+        <button>Add to Cart</button>
+    </div>
+
+</div>
+
+
+</body>
+</html>
